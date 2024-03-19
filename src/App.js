d94c7e8b94779551f7ffa13fc2ffd3edc7dd64b6
gabrielle-1 <73626380+gabrielle-1@users.noreply.github.com>
2024-03-19 17:29:52 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Router } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import Home from './pages/Home';
+import Ods from './pages/Ods';
+import Sobre from './pages/Sobre';
+
+function App(){
+  return(
+    <BrowserRouter>
+      <Router>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/ods' element={<Ods/>}/>
+        <Route path='/sobre' element={<Sobre/>}/>
+      </Router>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Ods.js
@@ -0,0 +1,9 @@
+function Ods(){
+    return(
+        <div>
+
+        </div>
+    );
+}
+
+export default Ods;
